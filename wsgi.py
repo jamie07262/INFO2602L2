d334c2110b9205a5b12718b6e42cec013783efbe
Franchesca James <157490201+jamie07262@users.noreply.github.com>
2025-02-17 15:32:50 +0000

    Added add-todo and toggle-todo command

--- wsgi.py
+++ wsgi.py
@@ -11,10 +11,10 @@ def initialize():
   db.create_all()
   bob = User('bob', 'bob@mail.com', 'bobpass')
   print(bob)
-  bob.todos.append(Todo('wash car'))
-  #new_todo = bob.create_todo('wash car')
+  #bob.todos.append(Todo('wash car'))
+  new_todo = bob.create_todo('wash car')
   db.session.add(bob)
-  #db.session.add(new_todo)
+  db.session.add(new_todo)
   db.session.commit()
   print('database intialized')
 
@@ -82,3 +82,32 @@ def get_user_todos(username):
     print(f'{username} not found!')
     return
   print(bob.todos)
+
+@app.cli.command('add-todo')
+@click.argument('username', default='bob')
+@click.argument('text', default='wash car')
+def add_task(username,text):
+  bob = User.query.filter_by(username=username).first()
+  if not bob:
+    print(f'{username} not found!')
+    return
+  new_todo = Todo(text)
+  bob.todos.append(new_todo)
+  db.session.add(bob)
+  db.session.commit()
+
+@click.argument('todo_id', default=1)
+@click.argument('username', default='bob')
+@app.cli.command('toggle-todo')
+def toggle_todo_command(todo_id,username):
+  user= User.query.filter_by(username=username).first()
+  if not user:
+    print(f'{username} not found!')
+    return
+
+  todo = Todo.query.filter_by(id=todo_id, user_id=user.id).first()
+  if not todo:
+    print(f'{username} has no todo id {todo_id}')
+
+  todo.toggle()
+  print(f'{todo.text} is {"done" if todo.done else "not done"}!')
